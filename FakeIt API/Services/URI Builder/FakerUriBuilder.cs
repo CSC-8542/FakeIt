f815498c8a968f2f1a0b379141891d80b6159c95
Nick Langan <55940428+W2NJL@users.noreply.github.com>
2021-11-29 13:47:02 -0500

    Implemented URIBuilder library

--- FakeIt API/Services/URI Builder/FakerUriBuilder.cs
+++ FakeIt API/Services/URI Builder/FakerUriBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using FakeIt_API.Entities;
 using System;
 using System.Collections.Generic;
+using System.Web;
 
 namespace FakeIt_API.Services.URIBuilder
 {
@@ -11,59 +12,34 @@ namespace FakeIt_API.Services.URIBuilder
 
         Dictionary<String, String> queryParamDictionary = new() { { "Quantity", "_quantity" }, { "Gender", "_gender" } };
 
-
-
         public FakerUriBuilder()
         {
         }
 
         public string GetPersonaUri(PersonaQuery query)
         {
+
             String baseURI = BaseURIOfFakerAPI + FakerAPIRequestType;
 
-            return MeshURI(baseURI, query);
+            var uriBuilder = new UriBuilder(baseURI);
+            var uriQuery = HttpUtility.ParseQueryString(uriBuilder.Query);              
 
+            Dictionary<string, string> queryParams = GenerateQueryParamAndValue(query);
 
-        }
+            foreach (string param in queryParams.Keys)            {
 
-        private string MeshURI(string baseURI, PersonaQuery query)
-        {
-            Dictionary<string, string> queryParams = GenerateQueryParams(query);
-
-            //ToDo: Hashmap, list of possible FakeIt parameters
-
-
-
-
-            foreach (string param in queryParams.Keys)
-            {
-
-
-                AddToURI(baseURI, queryParamDictionary.GetValueOrDefault(param), queryParams.GetValueOrDefault(param));
+                uriQuery[queryParamDictionary.GetValueOrDefault(param)] = queryParams.GetValueOrDefault(param);             
 
             }
 
+            uriBuilder.Query = uriQuery.ToString();
 
+            return uriBuilder.ToString();
 
+        }          
 
-
-            return baseURI;
-        }
-
-        private void AddToURI(string baseURI, string param, String paramValue)
-        {
-
-            baseURI = baseURI + "?" + param + "=" + paramValue;
-        }
-
-
-
-        private Dictionary<string, string> GenerateQueryParams(PersonaQuery query)
-        {
-            return AssessQuery(query);
-        }
-
-        private Dictionary<string, string> AssessQuery(PersonaQuery query)
+   
+        private Dictionary<string, string> GenerateQueryParamAndValue(PersonaQuery query)
         {
             Dictionary<string, string> queryMap = new();
             if (query != null)
